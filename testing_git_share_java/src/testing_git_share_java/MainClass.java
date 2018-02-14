bc1de0b5a6cbb60d93274800cd6fcc771e3391e7
dtklee <tkdas.cse@gmail.com>
2018-02-14 12:24:48 -0800

    testing from eclipse - 1

---
+++ testing_git_share_java/src/testing_git_share_java/MainClass.java
@@ -0,0 +1,11 @@
+package testing_git_share_java;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git working");
+
+	}
+
+}
