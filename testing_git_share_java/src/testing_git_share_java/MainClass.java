e0cdfba1c05b353f0a82b95033c52240dceb5f90
dtklee <tkdas.cse@gmail.com>
2018-02-14 12:31:27 -0800

    Commit 2. Just adding the second print statement.

--- testing_git_share_java/src/testing_git_share_java/MainClass.java
+++ testing_git_share_java/src/testing_git_share_java/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git working");
+		System.out.println("Editing committed file");
 
 	}
 
